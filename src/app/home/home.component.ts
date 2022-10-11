9ac99a4f841c04b64675bdbdd801215ae8abc87a
montoyafranco <francomontoya9810@gmail.com>
2022-10-11 00:34:38 -0300

    iframe andando arriba de todo trading view

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+declare const TradingView: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit ,AfterViewInit {
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(){
+    new TradingView.widget(
+      
+  {
+  "symbols": [
+    {
+      "proName": "FOREXCOM:SPXUSD",
+      "title": "S&P 500"
+    },
+    {
+      "proName": "FOREXCOM:NSXUSD",
+      "title": "US 100"
+    },
+    {
+      "proName": "FX_IDC:EURUSD",
+      "title": "EUR/USD"
+    },
+    {
+      "proName": "BITSTAMP:BTCUSD",
+      "title": "Bitcoin"
+    },
+    {
+      "proName": "BITSTAMP:ETHUSD",
+      "title": "Ethereum"
+    }
+  ],
+  "showSymbolLogo": true,
+  "colorTheme": "light",
+  "isTransparent": false,
+  "displayMode": "adaptive",
+  "locale": "es"
+}
+  
+
+);
+  }
+ 
+  
+
+}
