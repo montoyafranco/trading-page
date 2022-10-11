0131701c165b42538c64b63f6e9e00d93b12ef2d
montoyafranco <francomontoya9810@gmail.com>
2022-10-11 00:49:42 -0300

    funcionando solo con iframe arriba

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,39 +18,27 @@ export class HomeComponent implements OnInit ,AfterViewInit {
   }
   ngAfterViewInit(){
     new TradingView.widget(
-      
-  {
-  "symbols": [
-    {
-      "proName": "FOREXCOM:SPXUSD",
-      "title": "S&P 500"
-    },
-    {
-      "proName": "FOREXCOM:NSXUSD",
-      "title": "US 100"
-    },
-    {
-      "proName": "FX_IDC:EURUSD",
-      "title": "EUR/USD"
-    },
-    {
-      "proName": "BITSTAMP:BTCUSD",
-      "title": "Bitcoin"
-    },
-    {
-      "proName": "BITSTAMP:ETHUSD",
-      "title": "Ethereum"
+      {
+      "width": 980,
+      "height": 610,
+      "symbol": "NASDAQ:AAPL",
+      "timezone": "Etc/UTC",
+      "theme": "Light",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "withdateranges": true,
+      "range": "ytd",
+      "hide_side_toolbar": false,
+      "allow_symbol_change": true,
+      "show_popup_button": true,
+      "popup_width": "1000",
+      "popup_height": "650",
+      "no_referral_id": true,
+      "container_id": "tradingview_bac65"
     }
-  ],
-  "showSymbolLogo": true,
-  "colorTheme": "light",
-  "isTransparent": false,
-  "displayMode": "adaptive",
-  "locale": "es"
-}
-  
-
-);
+      );
   }
  
   
