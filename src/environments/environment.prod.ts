5815f27d00fe4627e6dd5df125cd19fc218e9b8d
montoyafranco <francomontoya9810@gmail.com>
2022-10-12 23:28:51 -0300

    cosas de firebase agregados y ayuda de fede

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebaseConfig :{
+    apiKey: "AIzaSyDNH-taAO8O_uwZ8RNs_KFwrcrpaFaSnP8",
+    authDomain: "trading-funding.firebaseapp.com",
+    projectId: "trading-funding",
+    storageBucket: "trading-funding.appspot.com",
+    messagingSenderId: "834863865972",
+    appId: "1:834863865972:web:f19fd7402d3244f820f121",
+    measurementId: "G-NE6QKDP978"
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyDNH-taAO8O_uwZ8RNs_KFwrcrpaFaSnP8",
+    authDomain: "trading-funding.firebaseapp.com",
+    projectId: "trading-funding",
+    storageBucket: "trading-funding.appspot.com",
+    messagingSenderId: "834863865972",
+    appId: "1:834863865972:web:f19fd7402d3244f820f121",
+    measurementId: "G-NE6QKDP978"
+  },
   production: false
 };
 
