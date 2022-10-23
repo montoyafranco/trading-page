1883191aa6fdd099cd0a9aa04668a6af1a5aae3d
montoyafranco <francomontoya9810@gmail.com>
2022-10-22 23:39:55 -0300

    probando paypal

--- src/app/transaccion/transaccion.component.ts
+++ src/app/transaccion/transaccion.component.ts
@@ -1,32 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import {render} from 'creditcardpayments/creditCardPayments'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var paypal: { Buttons: (arg0: { createOrder: (data: any, actions: any) => any; onApprove: (data: any, actions: any) => Promise<void>; onError: (err: any) => void; }) => { (): any; new(): any; render: { (arg0: any): void; new(): any; }; }; };
 
 @Component({
   selector: 'app-transaccion',
   templateUrl: './transaccion.component.html',
-  styleUrls: ['./transaccion.component.scss']
+  styleUrls: ['./transaccion.component.scss'],
 })
 export class TransaccionComponent implements OnInit {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef | undefined;
 
-  constructor() { 
+  product = {
+    price: 11000,
+    description: 'used couch, decent condition',
     
-    render({
-      id: "#myPaypalButtons",
-      currency: "USD",
-      value: "100.00",
-      onApprove:(details) =>{
-        alert("Transaccion exitosa")
+  };
 
-      }
-      
-    })
-      
+  paidFor = false;
 
-    
-
+  ngOnInit() {
+    paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: this.product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: this.product.price
+                }
+              }
+            ]
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          this.paidFor = true;
+          console.log(order);
+        },
+        onError: err => {
+          console.log(err);
+        }
+      })
+      .render(this.paypalElement?.nativeElement);
   }
-
-  ngOnInit(): void {
-  }
-
 }
