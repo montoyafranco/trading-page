a9415b9f94dff0f6537bffa21d6e36980c2c552f
montoyafranco <francomontoya9810@gmail.com>
2022-11-14 21:53:12 -0300

    tabla y nuevo fondo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ declare const TradingView2: any;
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit ,AfterViewInit {
+export class HomeComponent implements OnInit  {
 
   constructor(
     private auth: AuthService,
@@ -53,31 +53,8 @@ export class HomeComponent implements OnInit ,AfterViewInit {
       // this.router.navigate(['home']);
     });
   }
-  ngAfterViewInit(){
-    new TradingView.widget(
-      {
-      "width": "100%",
-      "height": 550,
-      "symbol": "NASDAQ:AAPL",
-      "timezone": "Etc/UTC",
-      "theme": "Light",
-      "style": "1",
-      "locale": "en",
-      "toolbar_bg": "#f1f3f6",
-      "enable_publishing": false,
-      "withdateranges": true,
-      "range": "ytd",
-      "hide_side_toolbar": false,
-      "allow_symbol_change": true,
-      "show_popup_button": true,
-      "popup_width": "300",
-      "popup_height": "400",
-      "no_referral_id": true,
-      "container_id": "tradingview_bac65"
-    }
-      );
-      
-  }
+  
+  
 
  
   
